1a331cd68daca9b1e464123a8b37e2d141d208de
shubhamshewdikar <shubham.shewdikar@gmail.com>
2020-05-08 23:12:30 +0530

    latest push 08-05-20

--- format.py
+++ format.py
@@ -1,41 +1,57 @@
 import pandas as pd
 import uuid
+from re import search
 
 df = pd.read_excel("data/inputfile-for-test-batch.xlsx")
 data = df['result']
+
+
 def data_format():
     arr = []
     for a in data:
         a = a.split('{')
 
-        latest_city = [x for x in a if x.startswith('city')]
-        latest_postcode = [x for x in a if x.startswith('postcode')]
-        latest_suburb = [x for x in a if x.startswith('suburb')]
-        road = [x for x in a if x.startswith('road')]
+        latest_city = [x for x in a if search('city', x)]
+        latest_postcode = [x for x in a if search('postcode', x)]
+        latest_suburb = [x for x in a if search('suburb', x)]
 
+        # road = [x for x in a if x.startswith('road')]
+        city_district = [x for x in a if x.startswith('city_district')]
+        state_district = [x for x in a if x.startswith('state_district')]
+        final = [x for x in a if not x.startswith('state') and not x.startswith('postcode') and not x.startswith('city')]
 
-        remaining = [x for x in a if not x.startswith('city')]
-        final = [x for x in remaining if not x.startswith('postcode')]
-        dct = {}
+        dct={}
         dct['id'] = str(uuid.uuid4())
 
+        address_str = ''
+
         if len(latest_suburb) > 0:
-            dct['suburb'] = latest_suburb[-1].split('suburb')[-1].strip()
+            dct['address'] = latest_suburb[-1].split('suburb')[-1].strip()
+        elif len(city_district) > 0:
+            dct['address'] = city_district[-1].split('city_district')[-1].strip()
+        elif len(state_district) > 0:
+            dct['address'] = state_district[-1].split('state_district')[-1].strip()
         else:
-            if road:
-                dct['suburb'] = road[-1].split('road')[-1].strip()
-            else:
-                dct['suburb'] = latest_city[-1].split('city')[-1].strip()
+            for i in final:
+                k = i.split(' ', 1)
+                if len(k) == 2:
+                    address_str += k[1]
+            dct['address'] = address_str
+
+
         if len(latest_city) > 0:
             dct['city'] = latest_city[-1].split('city')[-1].strip()
 
-        # address_str = ''
 
-        # for i in final:
-        #     k = i.split(' ', 1)
-        #     if len(k) == 2:
-        #         address_str += k[1]
-        #     dct["address"] = address_str.replace(']', '')
+
+        # if len(city_district) > 0:
+        #     address_str += city_district[-1].split('city_district')[-1].strip()
+        #
+        # if len(state_district) > 0:
+        #     address_str += state_district[-1].split('state_district')[-1].strip()
+
+        # dct["address"] = address_str.replace(']', '')
+
         if len(latest_postcode) > 1:
             dct['postcode'] = latest_postcode[-1].split('postcode')[-1].strip()
         else:
@@ -46,4 +62,4 @@ def data_format():
     return df2
 
 my_xl = data_format()
-my_xl.to_excel("test_final_v2.xlsx", sheet_name='test')
+my_xl.to_excel("demo_11.xlsx", sheet_name='test')
